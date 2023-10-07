0044f549d4be8e5d4f06d276df674313f30239a3
floington500 <zachappella@gmail.com>
2023-10-07 14:42:20 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 #
-# Allows you to select different components to install.
+# Allows you to select different components to set up.
 #
 
-install_kitty() {
+setup_kitty() {
 	echo "Setting up kitty configuration!"
-	ln kitty/ ~/.config/kitty
+
+	if ! command -v kitty &> /dev/null; then
+		echo "Kitty is not installed! Please install kitty before applying the configuration."
+		return 1
+	fi
+	ln -s $(pwd)/kitty/ ~/.config/kitty
 }
 
-install_awesome() {
-	
+setup_awesome() {
+	echo "Setting up awesome-wm configuration!"
+
+	if ! command -v awesome &> /dev/null; then
+		echo "Awesome is not installed! Please install awesome before applying the configuraton."
+	fi
+
+	cd awesome/
+	git submodule update --init --recursive
+
+	ln -s $(pwd)/awesome/ ~/.config/awesome
 }
 
 for component in "$@"; do
 	case "$component" in
 		kitty)
-			install_kitty
+			setup_kitty
 			;;
 		awesome)
-			install_awesome
+			setup_awesome	
 			;;
 		*)
 			echo "Invalid component $component" 
